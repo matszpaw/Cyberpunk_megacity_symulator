6470add07ba931210acbba46535c3cfac1e95a67
matszpaw <mat.sz.paw@gmail.com>
2023-07-07 16:38:22 +0200

    thing in itself

--- project/core.py
+++ project/core.py
@@ -1,14 +1,27 @@
 class thing_in_itself:
+    #what an universal object need to have?
+    #way of adding features
+    # list of features and way to activate them
+
+    def __init__(self,list_of_features:list):
+        self.list_of_features = list_of_features
+    
+    def add_feature(self,name_of_feature):
+        list_of_features.append(name_of_feature)
+        print('new amulet has entered the world of Ulram, beware of consequences...')
+    
+    def activate_features(self):
+        #add activation on tick
+        if len(list_of_features) != 0:
+        #reference to specyfic feature
+        else print('no features to activate')
 
 class features():
-    #basic ones
     #magical ones
-
 class tribes(thing_in_itself):
-
+    #basic features
 class place(thing_in_itself):
-
-class amulet(thing_in_itself):
+    #basic features
 
 
 #the "tick" is created by passing features into lists inside 
