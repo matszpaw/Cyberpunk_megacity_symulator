6b123c68867d2fdff1d5c10360192686d4016eb3
matszpaw <mat.sz.paw@gmail.com>
2023-07-11 11:54:18 +0200

    generates land objects now

--- project/core.py
+++ project/core.py
@@ -2,25 +2,29 @@ class thing_in_itself:
 
     def __init__(self,list_of_features:list):
         self.list_of_features = list_of_features
-    
+
     def add_feature(self,name_of_feature:str):
         self.list_of_features.append(name_of_feature)
-        print('new amulet has enter the world of Ulram, beware of the consequences...')
+        print('new feature added, beware of consequences...')
     
     def activate_features(self):
-        #add activation on tick
         if len(self.list_of_features) != 0:
-            print('features has been activated')
+            for x in range(len(self.list_of_features)):
+                print(f'feature {self.list_of_features[x]} has been activated')
         else:
             print('no features to activate')
 
+#every subclass has its own set of features in form of function
+#they are activated by passing their names into that function
+#add logic in which parameters of simillar/neighbur objects change
+#on activation
 
-
-thing1 = thing_in_itself([])
-
-
-print(thing1.list_of_features)
-thing1.activate_features()
-thing1.add_feature('strong')
-print(thing1.list_of_features)
-thing1.activate_features()
+#list_of_stuff = []
+#for x in range(2):
+#    list_of_stuff.append(thing_in_itself([]))
+#list_of_stuff[1].add_feature('strong')
+#print(list_of_stuff[1])
+#thing1 = thing_in_itself([])
+#thing1.add_feature('strong')
+#print(thing1.list_of_features)
+#thing1.activate_features()
--- project/world.py
+++ project/world.py
@@ -2,6 +2,44 @@ import random
 import numpy as np
 #simple world generator
 
+
+#this stuff will be in separate file
+kinds_of_tiles = ['lake','forest','meadows','marsh']
+list_of_events = ['goblins','tresure','inn','wandering merchant']
+
+class land_feature:
+    #when used transform string into object
+    def __init__(self,tile_name:str,encounters_good:list,encounters_bad:list):
+        self.tile_name = tile_name
+        self.encounters_good = encounters_good
+        self.encounters_bad = encounters_bad
+
+    def make_tile(self,tile_name:str):
+        if tile_name == 'meadows':
+            self.encounters_bad.append(list_of_events)
+            self.encounters_good.append(list_of_events)
+        if tile_name == 'forest':
+            self.encounters_bad.append(list_of_events)
+            self.encounters_good.append(list_of_events)
+        if tile_name == 'marsh':
+            self.encounters_bad.append(list_of_events)
+            self.encounters_good.append(list_of_events)
+        if tile_name == 'lake':
+            self.encounters_bad.append('fish')
+            self.encounters_good.append('fish')
+
+def ready_world_object(shape_of_world:list):
+    world = []
+    def row(z):
+        row = []
+        for i in range(z):
+            row.append(land_feature(kinds_of_tiles[random.randint(0,3)],[],[]))
+            row[i].make_tile(row[i].tile_name)
+        return row
+    for x in range(shape_of_world[1]):
+        world.append(row(shape_of_world[0]))
+    return np.array(world)
+
 def generator(shape_of_world:list):
     world = []
     kinds_of_tiles = ['lake','forest','meadows','marsh']
@@ -9,11 +47,3 @@ def generator(shape_of_world:list):
         x = [kinds_of_tiles[random.randint(0,3)] for i in range(shape_of_world[1])]
         world.append(x)
     return np.array(world)
-
-test = generator([2,2])
-print(test)
-
-#def reading_map(map):
-    #has to acces every tile on the map and then use function on them
-
-#def resources(tile:str):
